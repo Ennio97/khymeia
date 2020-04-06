523fdf7a8b4757114a4dbde8d72e4af96927a0b6
Ennio Michelon <ennio.michelon@crispybacon.it>
2020-04-06 09:58:10 +0200

    - Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,40 @@
+import {Component} from '@angular/core';
+
+import {NavController, Platform} from '@ionic/angular';
+import {SplashScreen} from '@ionic-native/splash-screen/ngx';
+import {StatusBar} from '@ionic-native/status-bar/ngx';
+import {Deeplinks} from '@ionic-native/deeplinks';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.scss']
+})
+export class AppComponent {
+    constructor(
+        private platform: Platform,
+        private splashScreen: SplashScreen,
+        private statusBar: StatusBar,
+        private deeplinks: Deeplinks,
+        private navController: NavController
+    ) {
+        this.initializeApp();
+    }
+
+    initializeApp() {
+        this.platform.ready().then(() => {
+            this.statusBar.styleDefault();
+            this.splashScreen.hide();
+            this.deeplinks.route({
+            }).subscribe(match => {
+                // match.$route - the route we matched, which is the matched entry from the arguments to route()
+                // match.$args - the args passed in the link
+                // match.$link - the full link data
+                console.log('Successfully matched route', match);
+            }, nomatch => {
+                // nomatch.$link - the full link data
+                console.error('Got a deeplink that didn\'t match', nomatch);
+            });
+        });
+    }
+}
